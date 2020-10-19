7646184cd64cf9f56743441290cf7b0c36db115c
Jermuli <49721423+Jermuli@users.noreply.github.com>
2020-10-19 21:12:18 +0300

    Create captcha.js

---
+++ captcha.js
@@ -0,0 +1,43 @@
+const apiUrl = 'https://075d9249.eu-gb.apigw.appdomain.cloud/captcha';
+var nonce;
+
+function getCAPTCHA(){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      var json = JSON.parse(this.responseText);
+      addPicture(json);
+    }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+  xmlHttp.send();
+}
+
+function addPicture(json){
+  var img = document.createElement("img");
+  nonce = json.nonce;
+  img.src = json.picture;
+  var src = document.getElementById("b2"); 
+  src.appendChild(img);
+}
+
+function isHuman(json){
+  if(json.passed == true){
+    document.getElementById("h1").innerHTML = "You are human";
+  }else{
+    document.getElementById("h1").innerHTML = "You could be robot";
+  }
+}
+
+function sendCAPTCHA(){
+  var xhttp = new XMLHttpRequest();
+	xhttp.addEventListener("readystatechange", function () {
+  		if (this.readyState === 4 && this.status == 200) {
+			json = JSON.parse(this.responseText);
+      isHuman(json);
+  		}
+	});
+
+	xhttp.open("POST", url, true);
+	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhttp.setRequestHeader("Cache-Control", "no-cache");
+}
