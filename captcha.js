da9c3813e74f514852b66a74ba78d06857d236c0
Jermuli <49721423+Jermuli@users.noreply.github.com>
2020-10-20 00:33:54 +0300

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -40,7 +40,7 @@ function sendCAPTCHA(){
 	});
 	var data = {text : document.getElementById("b2").value, nonce : confirmation};
 	
-	document.getElementById("b11").innerHTML = document.getElementById("b2").value;
+	document.getElementById("b1").innerHTML = document.getElementById("b2").value;
 	
 	xhttp.open("POST", apiUrl, true);
 	xhttp.setRequestHeader("Content-Type", "application/json");
